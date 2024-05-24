78585e6dee1ec35ce09628da8112aea0b58e01d0
Mario Castro <mcast032@gmail.com>
2024-05-23 23:04:38 -0700

    nav bar update

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Welcome!";
 
 document.querySelector("body").appendChild(h2);
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    function addContact() {
+        const contactButton = document.getElementById('btn2');
+        contactButton.addEventListener('click', function() {
+            let p = document.querySelector("p");
+            
+            if (!p) {
+                p = document.createElement("p");
+                document.body.appendChild(p);
+            }
+            
+            p.textContent = "323-978-3252";
+        });
+    }
+
+    addContact();
+});
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    function clearContact() {
+        const homeButton = document.getElementById('home-btn');
+        homeButton.addEventListener('click', function() {
+            const p = document.querySelector("p");
+            if (p) {
+                p.remove();
+            }
+        });
+    }
+
+    
+    clearContact();
+});
+
+
+
+
+
+
+const aboutButton = document.getElementById('btn1');
+aboutButton.addEventListener('click', function() {
+  alert('I am an aspiring Sofware Engineer');
+});
+
+
